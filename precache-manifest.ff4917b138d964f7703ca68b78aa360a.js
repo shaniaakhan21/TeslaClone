405f277a99ab9bc9425845b798bed673de2e9972
Shaniya Khan <57680699+shaniaakhan21@users.noreply.github.com>
2023-01-28 19:52:16 +0400

    Updates

---
+++ precache-manifest.ff4917b138d964f7703ca68b78aa360a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb537311dc7c90761d82",
+    "url": "/TeslaClone/static/css/main.a60d3dd0.chunk.css"
+  },
+  {
+    "revision": "fb537311dc7c90761d82",
+    "url": "/TeslaClone/static/js/main.6da5251f.chunk.js"
+  },
+  {
+    "revision": "2ecc0da3c0dd180d98ae",
+    "url": "/TeslaClone/static/js/runtime~main.54bbf485.js"
+  },
+  {
+    "revision": "15702e4d028812985559",
+    "url": "/TeslaClone/static/js/2.3963b260.chunk.js"
+  },
+  {
+    "revision": "5e1c4a9743c24123230f",
+    "url": "/TeslaClone/static/js/3.02c98a0d.chunk.js"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "/TeslaClone/static/media/logo.be622899.svg"
+  },
+  {
+    "revision": "369039deb6273cbee310713920ebde8b",
+    "url": "/TeslaClone/index.html"
+  }
+];
